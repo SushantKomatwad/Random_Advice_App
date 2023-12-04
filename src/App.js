c66e8cd2f99e80ff43fb56827ccac7293ab0a7a7
SushantKomatwad <136224535+SushantKomatwad@users.noreply.github.com>
2023-12-04 16:30:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [advice , setAdvice] = useState('');
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+
+      const { advice } = response.data.slip;
+      setAdvice(advice);
+
+    } catch (error) {
+      console.error('Error fetching advice:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='flex justify-center  items-center h-screen '>
+          <div className='shadow-2xl ring-4 ring-orange-500 bg-white w-[530px] h-[270px] rounded-2xl   flex flex-col justify-center '>
+             <h1 className='font-bold text-2xl text-gray-800  mx-16'><span className='text-black'>ADVICE : </span>{advice}</h1>
+             <div className='text-center mt-8'>
+              <button className='bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded-xl' onClick={fetchAdvice}>GIVE ME ADVICE !</button>
+             </div>
+          </div>
+      </div>  
+    </>
+  )
 }
 
-export default App;
+export default App
